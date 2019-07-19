d6de2b0fce5778afc0204ba6e4d27651d5329c7e
cameronwlewis <cameronwlewis@gmail.com>
2019-07-19 14:58:11 -0600

    added .yaml

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <basic-vue-chat :new-message="message"/>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import BasicVueChat from './components/basic-vue-chat/BasicVueChat.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'App',
+        components: {
+            BasicVueChat
+        },
+        data: function () {
+            return {
+                message: {}
+            }
+        }
+    }
 </script>
 
-<style>
-#app {
-  font-family: 'Comic Sans MS', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import
 
-Vue.config.productionTip = false
+Vue.use(require('vue-moment'));
+
+library.add(faPaperclip);
+library.add(faSmile);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    render: h => h(App)
+}).$mount('#app');
