484a8bc0c37b9a590289195e68b4c6ff38bd47c0
cameronwlewis <cameronwlewis@gmail.com>
2019-07-20 06:23:42 -0600

    nlp api working

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,12 +12,34 @@
 
 <script>
 import BasicVueChat from "basic-vue-chat";
+import axios from "axios";
 
 export default {
   el: "#home",
   name: "home",
   components: {
     BasicVueChat
+  },
+  created() {
+    let text =
+      "I hate people! Everybody is rotten to me and I don't have any friends. Even skunks avoid me.";
+    axios
+      .post(
+        "https://language.googleapis.com/v1beta2/documents:analyzeSentiment?key=AIzaSyArHVYdDqaLySgyQ6Md0SUiqJ1X54l_Epk",
+        {
+          document: {
+            content: text,
+            type: "PLAIN_TEXT"
+          },
+          encodingType: "UTF8"
+        }
+      )
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.log("There was an error:" + error.response);
+      });
   }
 };
 </script>
